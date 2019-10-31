4c1349b613425dee02e092da20fe52f841ba1ab5
temjkn <temjkn@gmail.com>
2019-10-31 13:42:39 +0300

    m

--- source/scripts/client.js
+++ source/scripts/client.js
@@ -72,25 +72,3 @@ function MainTriggersCarousel() {
 		};
 	};
 };
-
-//
-// $(function() {
-//     function cutLongText(textSize, finalLength) {
-//       $('.b-text__title').each(function() {
-//         if ($(this).text().length > textSize) {
-//           $(this).html($(this).text().slice(0, finalLength) + '...');
-//         }
-//       })
-//     }
-//     cutLongText(97, 97);
-//   });
-// $(function() {
-//     function cutLongText(textSize, finalLength) {
-//       $('.b-text__description').each(function() {
-//         if ($(this).text().length > textSize) {
-//           $(this).html($(this).text().slice(0, finalLength) + '...');
-//         }
-//       })
-//     }
-//     cutLongText(97, 201);
-//   });
